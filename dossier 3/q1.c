e539af048eb9e5a7635711f521e78f366f101f40
Harik med <hamouda.form@gmail.com>
2018-06-26 10:15:00 +0000

    dossier...

---
+++ dossier 3/q1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include<conio.h>
+ main()
+{
+int n;
+printf("entrer un nombre entier \n");
+scanf("%d",&n);
+printf("nombre entier est : %d \n",n);
+
+}
---
+++ dossier 3/q2.pl
@@ -0,0 +1,6 @@
+for (my $i = 1; $i <= 10; $i++) {
+	print "$i \n";
+}
+
+
+commande pour lancer le programme : perl test.pl
